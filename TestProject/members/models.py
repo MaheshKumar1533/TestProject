e0a759966d5af7afd496a6a97f871b44fb10ec4d
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2023-09-20 14:08:28 +0530

    Updated Sign Up Page

--- TestProject/members/models.py
+++ TestProject/members/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 
 class userDetails(models.Model):
+    name = models.CharField(max_length=30,default="name")
+    #email = models.CharField(max_length=50)
     number = models.IntegerField(max_length=11)
-    password = models.CharField(max_length=16)
+    password = models.CharField(max_length=16,default="hars1234")
--- TestProject/members/views.py
+++ TestProject/members/views.py
@@ -4,13 +4,17 @@ from django.template import loader
 from members.models import userDetails
 
 def members(request):
-    template = loader.get_template('index.html')
+    template = loader.get_template('signup.html')
     return HttpResponse(template.render())
 
 def submit(request):
-    number = int(request.GET["username"])
+    name = request.GET["username"]
+    #email = request.GET["email"]
+    number = int(request.GET["number"])
     password = request.GET["password"]
     record = userDetails()
+    record.name=name
+    #record.email = email
     record.number = number
     record.password = password
     record.save()
