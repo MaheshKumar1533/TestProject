9de49033dcb23822c3a0a7a8e50eedf3f3585465
V Mahesh Kumar <maheshkumarvmk0@gmail.com>
2023-09-20 07:55:17 +0530

    Added function to Store Login Details into DB

--- TestProject/members/models.py
+++ TestProject/members/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 
 class userDetails(models.Model):
-    number = models.BigIntegerField(max_length=11)
+    number = models.IntegerField(max_length=11)
     password = models.CharField(max_length=16)
--- TestProject/members/views.py
+++ TestProject/members/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from members.models import userDetails
 
 def members(request):
     template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+def submit(request):
+    number = int(request.GET["username"])
+    password = request.GET["password"]
+    record = userDetails()
+    record.number = number
+    record.password = password
+    record.save()
+    template = loader.get_template("result.html")
     return HttpResponse(template.render())
