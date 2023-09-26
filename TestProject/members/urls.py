61e6e32c74007e45976e0063635b418b076f0f18
V Mahesh Kumar <114634598+MaheshKumar1533@users.noreply.github.com>
2023-09-26 09:09:52 +0000

    need to add reverse for sign up

--- TestProject/members/urls.py
+++ TestProject/members/urls.py
@@ -4,10 +4,10 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.members, name='Choose UserType'),
-    path('signup', views.signup, name="Sign Up"),
-    path('login', views.login, name="Login"),
-    path('dashboard', views.dashboard, name="Dashboard"),
-    path('submit', views.submit, name='submit')
+    path('signup/', views.signup, name="sign Up"),
+    path('login/', views.login, name="login"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('submit/', views.submit, name='submit')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
